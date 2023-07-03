0de97f8fa6076a5d8a95e8501006291eb4d970d1
Fued Kraide <omekra@hotmail.com>
2023-07-03 13:55:56 +1000

    page structure setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import Landing from "./pages/Landing";
+import { Register, Dashboard, Landing, Error } from "./pages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Dashboard</div>} />
-        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
-        <Route path="*" element={<div>Error</div>} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
