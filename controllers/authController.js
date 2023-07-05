6e25b5c4d6f78649f501296975af99c730084c52
Fued Kraide <omekra@hotmail.com>
2023-07-05 10:12:31 +1000

    refactor errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,25 +1,6 @@
+import { BadRequestError } from "../errors/index.js";
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
-
-class CustomApiError extends Error {
-  constructor(message) {
-    super(message);
-  }
-}
-
-class BadRequestError extends CustomApiError {
-  constructor(message) {
-    super(message);
-    this.statusCode = StatusCodes.BAD_REQUEST;
-  }
-}
-
-class NotFoundError extends CustomApiError {
-  constructor(message) {
-    super(message);
-    this.statusCode = StatusCodes.NOT_FOUND;
-  }
-}
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
