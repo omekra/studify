fa902160e49f3c4254cac8c4a430006333b27acb
Fued Kraide <omekra@hotmail.com>
2023-07-05 15:24:07 +1000

    proxy setup

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
 const Dashboard = () => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch("/api/v1");
+      const data = await response.json();
+      console.log("🚀 ~ data:", data);
+    } catch (error) {
+      console.log("🚀 ~ error:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return <div>Dashboard</div>;
 };
 export default Dashboard;
