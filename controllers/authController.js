83b9bf0c893ef028cb6c895c7f02a91c43d5807a
Fued Kraide <omekra@hotmail.com>
2023-07-06 11:57:13 +1000

    login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-import { BadRequestError } from "../errors/index.js";
+import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
 
@@ -27,9 +27,29 @@ const register = async (req, res) => {
     location: user.location,
   });
 };
+
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    throw new UnAuthenticatedError("Invalid email");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticatedError("Invalid password");
+  }
+
+  const token = user.createJWT();
+  user.password = undefined;
+
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
+
 const updateUser = async (req, res) => {
   res.send("update user");
 };
