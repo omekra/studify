3ebece3e07738cbf55dce102cd7b035d2d5fcb5a
Fued Kraide <omekra@hotmail.com>
2023-07-06 14:47:45 +1000

    dashboard pages structure

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>dashboard</div>} />
+        <Route path="/">
+          <Route path="stats" element={<Stats />} />
+          <Route path="all-students" element={<AllStudents />} />
+          <Route path="add-student" element={<AddStudent />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
