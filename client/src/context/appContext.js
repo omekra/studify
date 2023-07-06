e7ba857ea54f0b808a8105f95680b914eaea18fd
Fued Kraide <omekra@hotmail.com>
2023-07-06 10:41:14 +1000

    persist user in local storage

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -9,15 +9,18 @@ import {
 } from "./actions";
 import axios from "axios";
 
+const token = localStorage.getItem("token");
+const user = localStorage.getItem("user");
+const userLocation = localStorage.getItem("location");
 const initialState = {
   isLoading: false,
   showAlert: false,
   alertText: "",
   alertType: "",
-  user: null,
-  token: null,
-  userLocation: "",
-  studentLocation: "",
+  user: user ? JSON.parse(user) : null,
+  token: token ? token : null,
+  userLocation: userLocation ? userLocation : "",
+  studentLocation: userLocation ? userLocation : "",
 };
 
 const AppContext = React.createContext();
@@ -36,19 +39,30 @@ const AppProvider = ({ children }) => {
     }, 3000);
   };
 
+  const addUserToLocalStorage = ({ user, token, location }) => {
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
+    localStorage.setItem("location", location);
+  };
+
+  const removeUserFromLocalStorage = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    localStorage.removeItem("location");
+  };
+
   const registerUser = async (currentUser) => {
     dispatch({ type: REGISTER_USER_BEGIN });
     try {
       const response = await axios.post("/api/v1/auth/register", currentUser);
-      // console.log("🚀 ~ response:", response);
       const { user, token, location } = response.data;
       dispatch({
         type: REGISTER_USER_SUCCESS,
         payload: { user, token, location },
       });
-      // localStorage to be added
+
+      addUserToLocalStorage({ user, token, location });
     } catch (error) {
-      // console.log("🚀 ~ error:", error.response);
       dispatch({
         type: REGISTER_USER_ERROR,
         payload: { msg: error.response.data.msg },
