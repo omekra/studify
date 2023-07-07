d6fdf702577bbd1770d15f563c6544a817934284
Fued Kraide <omekra@hotmail.com>
2023-07-07 11:24:55 +1000

    added react-icons package

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,13 @@
 import Wrapper from "../assets/wrappers/Navbar";
+import { FaHome } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>Navbar</h4>
+      <h4>
+        Navbar
+        <FaHome />
+      </h4>
     </Wrapper>
   );
 };
